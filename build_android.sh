2ad5964eeca3f00474f065b43199ebabcef8ebf7
Chen Yuheng <chyh1990@gmail.com>
2014-08-27 15:14:00 +0800

    add build_android.sh

---
+++ build_android.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+NDK_ROOT=/tmp/my-android-toolchain
+CROSS_COMPILE=$NDK_ROOT/bin/arm-linux-androideabi-
+export SYS_ROOT="$NDK_ROOT/sysroot"
+export CC="$CROSS_COMPILE"gcc
+export CXX="$CROSS_COMPILE"g++
+export LD="$CROSS_COMPILE"ld
+export AR="$CROSS_COMPILE"ar
+export RANLIB="$CROSS_COMPILE"ranlib
+export STRIP="$CROSS_COMPILE"strip
+
+./configure --host=arm-eabi --prefix=`pwd`/build --with-protoc=protoc
+make -j3 && make install
