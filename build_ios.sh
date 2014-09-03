864fa51ba6bec2355c9b0f179c3f8beb4cdf9068
Chen Yuheng <chyh1990@gmail.com>
2014-09-03 21:57:53 +0800

    Fix iOS script

--- build_ios.sh
+++ build_ios.sh
@@ -13,13 +13,15 @@ IPHONESIMULATOR_PLATFORM=${XCODEDIR}/Platforms/iPhoneSimulator.platform
 IPHONESIMULATOR_SYSROOT=${IPHONESIMULATOR_PLATFORM}/Developer/SDKs/${SIM_SDK}.sdk
 
 make distclean
-export CXXFLAGS="-fPIC -fvisibility=hidden -arch armv7 -arch armv7s -isysroot $IPHONEOS_SYSROOT"
+export CXXFLAGS="-fPIC -fvisibility=hidden -arch armv7 -arch armv7s \
+	-isysroot $IPHONEOS_SYSROOT -std=c++11 -stdlib=libc++"
 ./configure --build=armv7-apple-darwin13.0.0  --disable-shared \
-	#--prefix=`pwd`/build-OS --with-protoc=protoc
+	--prefix=`pwd`/build-OS --with-protoc=protoc
 make -j3 && make install
 
 make distclean
-export CXXFLAGS="-fPIC -fvisibility=hidden -arch i386 -isysroot $IPHONESIMULATOR_SYSROOT"
+export CXXFLAGS="-fPIC -fvisibility=hidden -arch i386 \
+	-isysroot $IPHONESIMULATOR_SYSROOT -std=c++11 -stdlib=libc++"
 export LDFLAGS="-arch i386 -isysroot $IPHONESIMULATOR_SYSROOT \
 -miphoneos-version-min=7.0"
 ./configure --build=i386-apple-darwin13.0.0  --disable-shared \
