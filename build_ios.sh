7320e1cf8c0f26ee2a25a8468ef82512b84a75c0
Chen Yuheng <chyh1990@gmail.com>
2014-10-21 22:17:33 +0800

    iOS: disable zlib

--- build_ios.sh
+++ build_ios.sh
@@ -13,10 +13,11 @@ IPHONESIMULATOR_PLATFORM=${XCODEDIR}/Platforms/iPhoneSimulator.platform
 IPHONESIMULATOR_SYSROOT=${IPHONESIMULATOR_PLATFORM}/Developer/SDKs/${SIM_SDK}.sdk
 
 make distclean
-export CXXFLAGS="-fPIC -fvisibility=hidden -arch armv7 -arch armv7s \
+export CXXFLAGS="-fPIC -fvisibility=hidden  \
+	-arch armv7 -arch armv7s -arch arm64 \
 	-isysroot $IPHONEOS_SYSROOT -std=c++11 -stdlib=libc++"
 ./configure --build=armv7-apple-darwin13.0.0  --disable-shared \
-	--prefix=`pwd`/build-ios-OS --with-protoc=protoc
+	--prefix=`pwd`/build-ios-OS --with-protoc=protoc --without-zlib
 make -j3 && make install
 
 make distclean
@@ -25,5 +26,6 @@ export CXXFLAGS="-fPIC -fvisibility=hidden -arch i386 \
 export LDFLAGS="-arch i386 -isysroot $IPHONESIMULATOR_SYSROOT \
 -miphoneos-version-min=7.0"
 ./configure --build=i386-apple-darwin13.0.0  --disable-shared \
-	--prefix=`pwd`/build-ios-SIMULATOR --with-protoc=protoc
+	--prefix=`pwd`/build-ios-SIMULATOR --with-protoc=protoc \
+	--without-zlib
 make -j3 && make install
